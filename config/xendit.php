991355257c88ba28aa03fc0cfb34cd17d252462c
zmiii1 <141105436+zmiii1@users.noreply.github.com>
2025-08-11 17:38:29 +0700

    Update xendit.php

--- config/xendit.php
+++ config/xendit.php
@@ -5,5 +5,5 @@ return [
     'public_key' => env('XENDIT_PUBLIC_KEY', ''),
     'callback_token' => env('XENDIT_CALLBACK_TOKEN', ''),
     'test_mode' => env('XENDIT_TEST_MODE', true),
-    'dev_mode' => env('XENDIT_DEV_MODE', env('APP_ENV') === 'local'),
+    'dev_mode' => env('XENDIT_DEV_MODE', env('APP_ENV') === 'production'),
 ];
